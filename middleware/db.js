08fcf24a087c8fd03a3ada6d9059afba32d766a3
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2024-03-06 01:12:55 +0530

    init

---
+++ middleware/db.js
@@ -0,0 +1,25 @@
+let mysql = require('mysql');
+
+
+
+let connection = mysql.createPool({
+    host     : process.env['DB_HOST'],
+    user     : process.env['DB_USER'],
+    password : process.env['DB_PASSWORD'],
+    database : process.env['DB_DATABASE'],
+    connectionLimit : 100,
+    charset: 'utf8mb4'
+  });
+  
+
+const mySqlQury =(qry)=>{
+    return new Promise((resolve, reject)=>{
+        connection.query(qry, (err, row)=>{
+            if (err) return reject(err);
+            resolve(row)
+        })
+    }) 
+}
+
+  
+module.exports = {connection, mySqlQury}
